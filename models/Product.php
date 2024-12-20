b953bea35bbb6ae14bfe4f6ff87d3040df31ba6a
youssefjalloul1 <163987751+youssefjalloul1@users.noreply.github.com>
2024-12-20 03:08:08 +0100

    please commit this

--- models/Product.php
+++ models/Product.php
@@ -1,43 +1,44 @@
 <?php
 class Product {
-    private $id;
-    private $name;
-    private $price;
-    private $stock;
-    private $idCategory;
-    private $image; 
+    private $ID_Product;
+    private $Name;
+    private $Price;
+    private $Stock;
+    private $ID_Category;
+    private $Image;
 
-    public function __construct($id, $name, $price, $stock, $idCategory, $image) {
-        $this->id = $id;
-        $this->name = $name;
-        $this->price = $price;
-        $this->stock = $stock;
-        $this->idCategory = $idCategory;
-        $this->image = $image; 
+    public function __construct($ID_Product, $Name, $Price, $Stock, $ID_Category, $Image = null) {
+        $this->ID_Product = $ID_Product;
+        $this->Name = $Name;
+        $this->Price = $Price;
+        $this->Stock = $Stock;
+        $this->ID_Category = $ID_Category;
+        $this->Image = $Image;
     }
 
     public function getId() {
-        return $this->id;
+        return $this->ID_Product;
     }
 
     public function getName() {
-        return $this->name;
+        return $this->Name;
     }
 
     public function getPrice() {
-        return $this->price;
+        return $this->Price;
     }
 
     public function getStock() {
-        return $this->stock;
+        return $this->Stock;
+    }
+
+    public function getCategoryId() {
+        return $this->ID_Category;
     }
 
     public function getImage() {
-        return $this->image; 
-    }
-
-    public function getIdCategory() {
-        return $this->idCategory;
+        return $this->Image;
     }
 }
+
 ?>
